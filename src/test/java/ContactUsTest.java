7cb7b3f894eeb56d9a0ddca3bf82cad2c301a814
Rafal Lesnicki <lesnickirafal@gmail.com>
2023-09-27 13:15:19 +0200

    d

---
+++ src/test/java/ContactUsTest.java
@@ -0,0 +1,78 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ContactUsTest {
+    private WebDriver driver; //komunikacja z przeglądarką
+
+    //    odpali sie przed wszystkimi testami
+    @BeforeAll
+    public static void setupClass(){
+        WebDriverManager.chromedriver().setup();
+    }
+
+    // przed każdym testem
+    @BeforeEach
+    void setupTest() {
+        driver = new ChromeDriver();
+    }
+
+    // po każdym teście
+    @AfterEach
+    void tearDown() {
+        if(driver != null){
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void shouldNotAllowToSendEmptyContactUsForm(){
+
+//        implicity wait - wait globalnym dla wszyskich elemenrów, nie polecam, działa tylko dla metod find element
+
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+//        Explicity wait - na jaki elemen chcemy poczekać, dużo więcej możliwości, czy element jest klikalny, widoczny,
+//        nie ustawiamy go globalnie dla wszystkich elementów ale dla elementu któty np wolniej się ładuje
+
+//        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+//        Fluent wait - rozszeżenie explicity waita
+
+
+        driver.get("http://www.automationpractice.pl/index.php");
+        WebElement contactUsLink = driver.findElement(By.cssSelector("#contact-link"));// //*[@id='contact-link']
+        contactUsLink.click();
+
+        WebElement sendButton = driver.findElement(By.cssSelector("#submitMessage"));// //*[@id='submitMessage']
+        sendButton.click();
+
+        WebElement redAlertBox = driver.findElement(By.className("alert-danger")); // //*[@class='alert alert-danger']
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10), Duration.ofMillis(250)); //będzie czekał przez 10 sekund, i odpytywał co 250 milissecond
+        wait.until(ExpectedConditions.visibilityOf(redAlertBox)); //poczekamy aż element będzie widoczny
+//        wait.until(d -> redAlertBox.isDisplayed()); //poczekamy aż element będzie widoczny
+
+        assertThat(redAlertBox.isDisplayed()).isTrue(); // sprawdzu czy element jest wyświetlony
+    }
+
+    public void ShouldNotAllowToSendContactUsFormWithEmailOnly(){
+
+    }
+
+
+}
---
+++ src/test/java/HomPageTest.java
@@ -0,0 +1,67 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class HomPageTest {
+
+    private WebDriver driver; //komunikacja z przeglądarką
+    private WebElement element; //obsługa elementów na stronie
+
+    //    odpali sie przed wszystkimi testami
+    @BeforeAll
+    public static void setupClass(){
+        WebDriverManager.chromedriver().setup();
+    }
+
+// przed każdym testem
+    @BeforeEach
+    void setupTest() {
+        driver = new ChromeDriver();
+    }
+
+    // po każdym teście
+    @AfterEach
+    void tearDown() {
+       if(driver != null){
+           driver.quit();
+       }
+    }
+
+    @Test
+    public void shouldReturnCorrectPageTitle(){
+        driver.get("http://www.automationpractice.pl/index.php");
+
+        Assertions.assertThat(driver.getTitle()).isEqualTo("My Shop");
+    }
+
+    public void clickOnBestsellersButton() throws InterruptedException {
+        driver.findElement(By.cssSelector("#home-page-tabs .blockbestsellers")).click();
+    }
+
+    @Test
+    public void shouldSeeBestsellersItemsOnHomePage() throws InterruptedException {
+        driver.get("http://www.automationpractice.pl/index.php");
+        clickOnBestsellersButton();
+        List<WebElement> productNamesByCssSelector = driver.findElements(By.cssSelector("#blockbestsellers .product-name"));
+//        xpath
+//        List<WebElement> productNamesByCssXpath = driver.findElements(By.xpath("//*[@id='blockbestsellers']//*[@class='product-name']"));
+
+        List<WebElement> productWithEmptyName = productNamesByCssSelector.stream()
+                .filter(el -> el.getText().isEmpty())
+                .collect(Collectors.toList());
+
+        assertThat(productWithEmptyName).isEmpty();
+    }
+}
